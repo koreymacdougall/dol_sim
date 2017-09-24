f52457b6b5b49776758f1dc304b659ed6d266e80
koreymacdougall <korey@koreymacdougall.com>
2017-09-24 00:57:59 -0400

    Update harvesting logic

--- agent.py
+++ agent.py
@@ -6,21 +6,28 @@ class Agent():
         Knowledge interference levels?
     """
 
-
 #global world_size
 
-    def __init__(self, x, y, agent_id, world_squares, harvest_duration):
+    def __init__(self, x, y, id, world_squares, harvest_duration,
+            refine_duration, learning_rate):
         world_squares = world_squares
         self.x = x
         self.y = y
-        # use next method, iterator fn, seems like ruby's first fn
-        self.position = next((square for square in world_squares if square.x ==
+        # next method, iterator fn, seems like ruby's first fn
+        self.position = next((square for square in world_squares if square.x ==\
             self.x and square.y == self.y), None)
         print (self.x, self.y, self.position.x, self.position.y)
-        self.agent_id = agent_id
-        self.inventory = {}
+        self.id = id
+        self.inventory = []
+        # harvest duration may be a sim param, may be a skill param, that can
+        # vary per agent
         self.harvest_duration = harvest_duration
+        self.refine_duration = refine_duration
         self.action = ""
+        # learning rate will determine how quickly skills improve
+        # higher rate, sooner to reach the next level of master
+        # Note - this will be a step-wise learning model, with discrete jumps
+        self.learning_rate = learning_rate
 
         #memory to be built
         memory = {}
@@ -31,35 +38,34 @@ class Agent():
     # note that world wraps around on both axes
 
     def move_left(self, sim_params):
-        print("Moving agent", self.agent_id, "left")
+        print("Moving agent", self.id, "left")
         if self.x - 1 >= 0:
             self.x -= 1
         else:
             self.x = sim_params.world_size - 1
 
     def move_right(self, sim_params):
-        print("Moving agent", self.agent_id, "right")
+        print("Moving agent", self.id, "right")
         if self.x + 1 <= sim_params.world_size - 1:
             self.x += 1
         else:
             self.x = 0
 
     def move_down(self, sim_params):
-        print("Moving agent", self.agent_id, "down")
+        print("Moving agent", self.id, "down")
         if self.y - 1 >= 0:
             self.y -= 1
         else:
             self.y = sim_params.world_size - 1
 
     def move_up(self, sim_params):
-        print("Moving agent", self.agent_id, "up")
+        print("Moving agent", self.id, "up")
         if self.y + 1 <= sim_params.world_size - 1:
             self.y += 1
         else:
             self.y = 0
 
     # agent resource action definitions #######################################
-
     def harvest(self, world_squares, round):
         if self.position.square_resource:
             print("harvesting resource: ", self.position.square_resource)
@@ -78,10 +84,10 @@ class Agent():
     # move_list defines how agents can move around the map
     move_list = [move_left, move_right, move_down, move_up]
 
-    # resource_action_list defines what agents can do with resources
+    # resource_actions defines what agents can do with resources
     # for now, resource action list is only harvest
-    resource_action_list = [harvest]
-    #resource_action_list = [harvest, trade, refine]
+    resource_actions = [harvest]
+    #resource_actions = [harvest, trade, refine]
 
     # skill_list
     # skill_list = []
@@ -89,8 +95,8 @@ class Agent():
 
     # full action list is a combination of all the possible actions:
     # move & resource action for now
-    full_action_list = [move_list, resource_action_list]
+    full_action_list = [move_list, resource_actions]
 
     # print agent position to screen
     def print_position(self):
-      print("Agent ", self.agent_id, " position x,y:", self.x, self.y)       
+      print("Agent ", self.id, " position x,y:", self.x, self.y)       
--- runner.py
+++ runner.py
@@ -13,10 +13,15 @@ class SimParams():
         self.num_agents = num_agents
         self.resources = resources
 
-# create sim_param instance to hold class variables
-# this is main line(s) to set up sim-wide params
+# Main line(s) to set up sim-wide params
 
-# this is to be set by experimenter; resources, below, are what's used in sim
+# this list to be set by experimenter; resources, below, are what's used in sim
+# resource frequencies will be variableized
+# resource density will be determined by:
+# set total avail, then randomly assign subsets of the resource
+# eg total = 0.4, a = rand(0, 0.4), total_rem = total - 0 - a.freg
+# repeat for all reso's, could say that if early ones take up entire range, 
+# other ones get some super small value (0.01 or s/t)
 resources_list = [
         ['resource a', round(Decimal(0.1), 2)],
         ['resource b', round(Decimal(0.1), 2)],
@@ -46,17 +51,21 @@ for reso in resources_list:
 # instantiate an instance to store global simulation parameters
 sim_params = SimParams(world_size=5, num_agents=2, resources=resources)
 
-# Build world
+# Build world, keep track of resource numbers
 world_squares = world_builder_fn(sim_params.world_size, sim_params.resources)
-resource_counter = 0
+initial_raw_resource_count = 0
+raw_resource_count = 0
+harvested_resource_count = 0
 print("World Squares:")
 print(world_squares)
 for s in world_squares:
     print(s.square_resource, s.x, s.y)
     if s.square_resource:
-        resource_counter += 1
+        raw_resource_count += 1
+        initial_raw_resource_count += 1
 
-print("Total num resources: ", resource_counter)
+print("Total num raw resources: ", raw_resource_count)
+print("Total num harvested resources: ", harvested_resource_count)
 
 # Setup Agents
 agent_list=[]
@@ -64,14 +73,16 @@ for i in range(sim_params.num_agents):
     agent_list.append(Agent(
         x=random.choice(range(sim_params.world_size)),
         y=random.choice(range(sim_params.world_size)),
-        agent_id=i,
+        id=i,
         world_squares = world_squares,
         harvest_duration = 2,
+        refine_duration = 5,
+        learning_rate = 1
         ))
 
 
 # number of turns for a single run
-sim_params.round_length = 5
+sim_params.round_length = 500
 
 # End World Setup
 ################################################################################
@@ -82,36 +93,47 @@ for round in range(sim_params.round_length):
     print("====================")
     for agent in agent_list:
         agent.print_position()
-        print(agent.action)
+        print(agent.id, " currently doing: ", agent.action)
         # grab agents' current square from world_squares
         # "next" grabs matching instance from iterator
         agent.position = next((square for square in world_squares if square.x == agent.x and
             square.y == agent.y), None)
+
         # main action: choose between moving, manipulating resources,
         # or something else (TBD); currently random, TODO: will change
         # sub action : choose a sub-action
         # e.g., move left, move up, harvest resource, trade resource, etc
-
+        #this else needs to be refactored, redundant with lines below
         if agent.action == "":
             main_action = random.choice(agent.full_action_list)
-        else:
-            main_action = agent.resource_action_list
 
-        if main_action == agent.resource_action_list:
+        if main_action == agent.resource_actions:
             # i.e., if agent is choosing to do something with resources
             # then pass world_squares as an argument, round num for counter
             if agent.action == "harvesting":
-                print ("agent is harvesting!")
-                print ("harvesting started on round:", agent.action_start_time)
-                print ("harvesting will end on round:", agent.action_end_time)
-                if round >= agent.action_end_time:
-                    print("agent has finished harvesting", agent.position.square_resource)
-                    quit()
+                if agent.position.square_resource:
+                    print ("agent is harvesting!")
+                    print ("harvesting started on round:", agent.action_start_time)
+                    print ("harvesting will end on round:", agent.action_end_time)
+                    if round > agent.action_end_time:
+                        print("agent has finished harvesting",\
+                                agent.position.square_resource["name"])
+                        agent.inventory.append(agent.position.square_resource)
+                        agent.position.square_resource = None
+                        harvested_resource_count += 1
+                        raw_resource_count -= 1
+                        agent.action = ""
+                    else:
+                        print("agent is still harvesting", agent.position.square_resource)
                 else:
-                    print("agent is still harvesting", agent.position.square_resource)
+                    #if in harvesting but no resource
+                    print("agent tried to harvest, but no resources here")
 
+            # if agent is doing a resource action besides harvesting (currently
+            # aren't any such actions, pick randomly)
             else:
                 sub_action = random.choice(main_action)(agent, world_squares, round)
+
         elif main_action == agent.move_list:
             # i.e., if agent is moving, pass sim params to access world_size
             sub_action = random.choice(main_action)(agent, sim_params)
@@ -125,3 +147,6 @@ for round in range(sim_params.round_length):
 
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("Simulation complete")
+print("Num raw resources initially: ", initial_raw_resource_count)
+print("Num raw resources remaining: ", raw_resource_count)
+print("Num harvested resources : ", harvested_resource_count)
